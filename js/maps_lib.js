81343f7baf3b74eb6907fe0939b2a60e8b4e43c5
Jack Dougherty <jack.dougherty@trincoll.edu>
2014-08-11 21:26:19 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -328,6 +328,7 @@ var MapsLib = {
     var columns = json["columns"];
     var rows = json["rows"];
     var template = "";
+    alert("GOT HERE1.");
 
     var results = $("#listview");
     results.empty(); //hide the existing list and empty it out first
@@ -335,7 +336,6 @@ var MapsLib = {
     if (rows == null) {
       //clear results list
       results.append("<span class='lead'>No results found</span>");
-      alert("Row is null.");
       }
     else {
 
